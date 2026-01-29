d89657b0fb39f81df2fedebcdf55fe92c60432f3
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-29 11:30:55 +0200

    added endpoints

--- app/main.py
+++ app/main.py
@@ -16,28 +16,28 @@ def customers_credit_limit_outliers():
 
 @app.get("/q2/orders-null-comments")
 def orders_null_comments():
-    pass
+    dal.get_orders_with_null_comments()
 
 @app.get("/q3/customers-first-5")
 def customers_first_5():
-    pass
+    dal.get_first_5_customers()
 
 @app.get("/q4/payments-total-average")
 def payments_total_average():
-    pass
+    dal.get_payments_total_and_average()
 
 @app.get("/q5/employees-office-phone")
 def employees_office_phone():
-    pass
+    dal.get_employees_with_office_phone()
 
 @app.get("/q6/customers-shipping-dates")
 def customers_shipping_dates():
-    pass
+    dal.get_customers_with_shipping_dates()
 
 @app.get("/q7/customer-quantity-per-order")
 def customer_quantity_per_order():
-    pass
+    dal.get_customer_quantity_per_order()
 
 @app.get("/q8/customers-payments-by-lastname-pattern")
 def customers_payments_by_lastname_pattern(pattern: str = "son"):
-    pass
+    dal.get_customers_payments_by_lastname_pattern()
